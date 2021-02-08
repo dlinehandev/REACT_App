0cb9685766b276bb15f60b2b385a4c4993f2366e
Daniel Linehan <daniellinehan@gmail.com>
2021-02-08 17:08:17 +0000

    deleting tasks

--- src/App.js
+++ src/App.js
@@ -24,10 +24,15 @@ const App = () => {
     },
   ])
 
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id ))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks to display'}
     </div>
   );
 }
